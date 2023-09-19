b5f84e209bb3da576a1825c0c05c5ac79b0bf758
Erick <erickcestari03@gmail.com>
2023-09-19 17:20:52 -0300

    compiling hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::fs;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct File {
+    name: String,
+    expression: Term,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Print {
+    value: Box<Term>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Int {
+    value: i32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Str {
+    value: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "kind")]
+pub enum Term {
+    Int(Int),
+    Str(Str),
+    Print(Print)
+}
+#[derive(Debug)]
 pub enum Val {
     Void,
     Int(i32),
@@ -5,10 +38,30 @@ pub enum Val {
     Str(String),
 }
 
-fn eval(program: Program) -> Val {
-    todo!("eval")
+fn eval(term: Term) -> Val {
+    match term {
+        Term::Int(number) => Val::Int(number.value),
+        Term::Str(string) => Val::Str(string.value),
+        Term::Print(print) => {
+            let val = eval(*print.value);
+
+            match val {
+                Val::Int(n) => print!("{n}"),
+                Val::Bool(b) => print!("{b}"),
+                Val::Str(s) => print!("{s}"),
+                _ => panic!("Cannot print void"),
+            };
+            Val::Void
+        }
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let program = fs::read_to_string("./examples/hello.json").unwrap();
+    let program : File = serde_json::from_str::<File>(&program).unwrap();
+    
+    let term = program.expression;
+    eval(term);
+
+    
 }
